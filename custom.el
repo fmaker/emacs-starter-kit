173a9c159902eba2d19f24c5c81b91fcd6fde54a
Frank Maker <frank.maker@gmail.com>
2011-05-08 21:06:16 -0700

    Set linux as default c mode

--- custom.el
+++ custom.el
@@ -5,6 +5,7 @@
   ;; If there is more than one, they won't work right.
  '(backup-by-copying t)
  '(backup-directory-alist (quote (("." . "~/.saves"))))
+ '(c-default-style (quote ((c-mode . "linux"))))
  '(desktop-save t)
  '(desktop-save-mode t)
  '(global-hi-lock-mode nil)
@@ -21,3 +22,9 @@
  '(vc-follow-symlinks t)
  '(vc-make-backup-files nil))
 
+(custom-set-faces
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ )
